6891e5dc6c9241d9da2741bd1fa9d33074a49c68
yangzzang89 <ymh7701@gmail.com>
2022-05-10 16:30:29 +0900

    Lombok 테스트 커밋

---
+++ src/test/java/com/jojoIdu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package com.jojoIdu.book.springboot.dto;
+
+import com.jojoIdu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        String name = "test";
+        int amount = 100;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
