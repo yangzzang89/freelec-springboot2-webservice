6f879ad16005b9e2e548d359d7740df7b1d1b3df
yangzzang89 <ymh7701@gmail.com>
2022-05-11 16:43:20 +0900

    퇴근 전 백업

---
+++ src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,20 @@
+package com.jojoIdu.book.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+
+}
