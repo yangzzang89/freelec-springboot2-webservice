17cff2aae77a79bbbaa7856f4254116d78eb3ae7
yangzzang89 <ymh7701@gmail.com>
2022-06-29 17:49:49 +0900

    2022/06/29 수정 Commit.

--- src/main/java/com/jojoIdu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoIdu/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.jojoIdu.book.springboot.service.posts;
 
 import com.jojoIdu.book.springboot.domain.posts.Posts;
 import com.jojoIdu.book.springboot.domain.posts.PostsRepository;
+import com.jojoIdu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoIdu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoIdu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoIdu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,10 +10,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회기능만 가능하게 설정
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
--- src/main/java/com/jojoIdu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/IndexController.java
@@ -1,18 +1,39 @@
 package com.jojoIdu.book.springboot.web;
 
+import com.jojoIdu.book.springboot.service.posts.PostsService;
+import com.jojoIdu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoIdu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping(value = "/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+
         return "index";
     }
 
     @GetMapping({"/posts/save"})
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        
+        return "posts-update";
     }
 }
