da2decb1a590ed6d76396476c627bc3d106aee15
yangzzang89 <ymh7701@gmail.com>
2022-07-01 14:33:39 +0900

    2022/06/29 4장_삭제 Commit.

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -6,7 +6,10 @@ var main = {
         });
         $("#btn-update").on('click',function(){
             _this.update();
-        })
+        });
+        $("#btn-delete").on('click',function(){
+           _this.delete();
+        });
     },
     save : function(){
         var data = {
@@ -47,6 +50,21 @@ var main = {
             window.location.href = '/';
         }).fail(function(error){
            alert(JSON.stringify(error));f
+        });
+    },
+    delete : function(){
+        var id = $('#id').val();
+
+        $.ajax({
+            type : 'DELETE',
+            url : '/api/v1/posts/' + id,
+            dataType : 'json',
+            contentType : 'application/json; charset=utf-8'
+        }).done(function (){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
         });
     }
 };
